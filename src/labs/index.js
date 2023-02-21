0c83cdb3f91ab6f7f76eff37aee64bb0dfad9d33
KemingXu <273638354@qq.com>
2023-02-20 21:43:24 -0500

    2.9 ok

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,7 +1,9 @@
 import Assignment6 from "./a6";
+import Nav from "../nav";
 function Labs() {
   return (
     <div>
+      <Nav />
       <Assignment6 />
     </div>
   );
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,6 +1,8 @@
+import Nav from "../nav";
 function Tuiter() {
   return (
     <div>
+      <Nav />
       <h1>Tuiter</h1>
     </div>
   );
