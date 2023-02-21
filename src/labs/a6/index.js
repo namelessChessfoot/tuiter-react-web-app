0fa4b2d718404399056d1b834347cd97b51416c4
KemingXu <273638354@qq.com>
2023-02-20 22:08:28 -0500

    a6 labs done

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,15 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
 function Assignment6() {
   return (
     <div>
       <h1>Assignment 6</h1>
+      <ConditionalOutput />
+      <Classes />
+      <Styles />
+      <TodoList />
     </div>
   );
 }
