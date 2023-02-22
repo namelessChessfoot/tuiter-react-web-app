49a20fc2e136083c35f22999594e309b21d57970
KemingXu <273638354@qq.com>
2023-02-22 01:04:31 -0500

    try solving page not found

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+const HomeComponent = () => {};
+
+export default HomeComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,6 +2,7 @@ import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import ExploreComponent from "./explore";
+import { Routes, Route } from "react-router";
 function Tuiter() {
   return (
     <div className="row mt-2">
@@ -12,6 +13,10 @@ function Tuiter() {
         className="col-10 col-md-10 col-lg-7 col-xl-6"
         style={{ position: "relative" }}
       >
+        {/* <Routes>
+          <Route path="home"    element={<HomeComponent/>}/>
+          <Route path="/explore" element={<ExploreComponent />} />
+        </Routes> */}
         <ExploreComponent />
       </div>
       <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
