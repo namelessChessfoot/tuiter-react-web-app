73e1e922456d5c4c26742c8388dc8e56bceefd6b
KemingXu <273638354@qq.com>
2023-03-25 18:30:05 -0400

    okok

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-// const TUITS_API = "http://localhost:4000/api/tuits";
-// const TUITS_API = "https://tuiter-node-server-app-7up7.onrender.com/api/tuits";
 const API_BASE = process.env?.REACT_APP_API_BASE || "http://localhost:4000/api";
 const TUITS_API = `${API_BASE}/tuits`;
 
